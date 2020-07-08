abdb6695fdcfbd19351177892f820811e71ac744
Adi Skop <adiskop@gmail.com>
2020-07-08 13:33:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,9 @@ end
 
 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls (calls)
+  calls.any? {|call|call.length>4}
+
 end
 
 def find_the_cheese# code an argument here
